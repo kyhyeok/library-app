aa16b61826d6a066d6e362eb1ef03c6d3e297ea8
김윤혁 <kyhyeok@gmail.com>
2025-08-28 15:15:16 +0900

    Junit5 사용법과 테스트하기

---
+++ src/test/kotlin/com/group/libraryapp/calculator/JunitCalculatorTest.kt
@@ -0,0 +1,76 @@
+package com.group.libraryapp.calculator
+
+import com.group.libraryapp.calulator.Calculator
+import org.assertj.core.api.AssertionsForInterfaceTypes.assertThat
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
+
+class JunitCalculatorTest {
+
+    @Test
+    fun add() {
+        // given
+        val calculator = Calculator(5)
+
+        // when
+        calculator.add(3)
+
+        // then
+        assertThat(calculator.number).isEqualTo(8)
+    }
+
+    @Test
+    fun minus() {
+        // given
+        val calculator = Calculator(5)
+
+        // when
+        calculator.minus(3)
+
+        // then
+        assertThat(calculator.number).isEqualTo(2)
+    }
+
+    @Test
+    fun multiply() {
+        // given
+        val calculator = Calculator(5)
+
+        // when
+        calculator.multiply(3)
+
+        // then
+        assertThat(calculator.number).isEqualTo(15)
+    }
+
+    @Test
+    fun divide() {
+        // given
+        val calculator = Calculator(5)
+
+        // when
+        calculator.divide(2)
+
+        // then
+        assertThat(calculator.number).isEqualTo(2)
+    }
+
+    @Test
+    fun divideException() {
+        // given
+        val calculator = Calculator(5)
+
+        // when & then
+        val message = assertThrows<IllegalArgumentException> {
+            calculator.divide(0)
+        }.message
+        assertThat(message).isEqualTo("0으로 나눌 수 없습니다")
+
+        // when & then
+        assertThrows<IllegalArgumentException> {
+            calculator.divide(0)
+        }.apply {
+            assertThat(message).isEqualTo("0으로 나눌 수 없습니다")
+        }
+    }
+}
